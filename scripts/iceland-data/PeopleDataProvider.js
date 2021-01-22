0a732be32baa6bdc0e3643006e989d9b083d3445
Sophia Spaulding <soapsstone@gmail.com>
2021-01-22 11:13:26 -0500

    last changes3

--- scripts/iceland-data/PeopleDataProvider.js
+++ scripts/iceland-data/PeopleDataProvider.js
@@ -6,31 +6,10 @@ const peopleCollection = [
         name: "Vigdís Finnbogadóttir: Former President, 1980-1996"
     },
     {
-        name: "Halldór Laxness: Writer of poetry, newspaper articles, plays, short stories and novels"
-    },
-    {
-        name: "Halldór Laxness: Writer of poetry, newspaper articles, plays, short stories and novels"
-    },
-    {
         name: "Bjarni Tryggvason, Astronaut and Scientist"
-    },
-    {
-        name: "Gylfi Sigurðsson: Footballer"
-    },    
-    {
-        name: "Stefan Karl Stefansson: Actor"
-    },
-    {
-        name: "Kolbeinn Sigþórsson: Footballer"
-    },
+    },  
     {
         name: "Hafþór Júlíus Björnsson: Professional strongman"
-    },
-    {
-        name: "Arnaldur Indriðason: Writer"
-    },
-    {
-        name: "Ólafur Jóhann Ólafsson: Writer and businessman"
     },
 ]
 
